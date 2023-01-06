a4abbdda47edb476a029b6eb188144f1f7e3ac72
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-01-06 22:51:11 +0900

    :sparkles: :: User Entity 생성

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,7 +1,35 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Exclude } from "class-transformer";
+import { IsEmail, Length } from "class-validator"
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, OneToMany, BeforeInsert } from "typeorm"
 
-@Entity()
-export class User {
+@Entity("users")
+export default class User extends BaseEntity {
+    @Index()
+    @IsEmail(undefined,{message:"메일 주소가 잘못 되었습니다."})
+    @Length(1,255,{message:"메일 주소는 변경이 불가합니다."})
+    @Column({unique:true})
+    email: string;
+
+    @Index()
+    @Length(2,32,{message:"사용자 이름은 2자 이상이어야 합니다"})
+    @Column({unique:true})
+    username: string;
+
+    @Exclude()
+    @Column()
+    @Length(6,255,{message:"비밀번호는 6자리 이상이어야 합니다."})
+    password: string;
+
+    @OneToMany(() => Post,(post) => post.user)
+    posts:Post[];
+
+    @OneToMany(() => Vote,(vote) => vote.user)
+    votes.Vote[];
+
+    @BeforeInsert()
+    async function hashPassword() {
+        this.password = await bcrypt.hash(this.password,6);
+    }
 
     @PrimaryGeneratedColumn()
     id: number
