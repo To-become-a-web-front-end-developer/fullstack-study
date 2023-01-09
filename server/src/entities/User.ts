8c4e169cc53cc0859774906e0ba8b4fcc6d89f25
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-01-09 23:05:16 +0900

    :memo: :: User Entity 주석 추가 및 필요없는 부분 제거

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,12 +1,17 @@
 import { Exclude } from "class-transformer";
 import { IsEmail, Length } from "class-validator"
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, OneToMany, BeforeInsert } from "typeorm"
+import bcrypt from "bcryptjs";
 
+// 데코레이터 클래스는 User 클래스가 Entity임을 나타내는 데 사용된다. CREATE TABLE user 부분과 같다.
 @Entity("users")
 export default class User extends BaseEntity {
+    // 정보를 찾을 때 빨리 찾기 위해 Index를 사용함.
+    // 검색의 빈도가 높을 때 사용하기.
     @Index()
     @IsEmail(undefined,{message:"메일 주소가 잘못 되었습니다."})
-    @Length(1,255,{message:"메일 주소는 변경이 불가합니다."})
+    @Length(1,255,{message:"메일 주소는 반드시 입력해야 합니다."})
+    // 값이 유일해야함.
     @Column({unique:true})
     email: string;
 
@@ -17,9 +22,10 @@ export default class User extends BaseEntity {
 
     @Exclude()
     @Column()
-    @Length(6,255,{message:"비밀번호는 6자리 이상이어야 합니다."})
+    @Length(8,255,{message:"비밀번호는 8자리 이상이어야 합니다."})
     password: string;
 
+    // 관계와 관련된 부분
     @OneToMany(() => Post,(post) => post.user)
     posts:Post[];
 
@@ -30,17 +36,5 @@ export default class User extends BaseEntity {
     async function hashPassword() {
         this.password = await bcrypt.hash(this.password,6);
     }
-
-    @PrimaryGeneratedColumn()
-    id: number
-
-    @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
 
 }
